6db04b67a58842ebabe04dddb29f08cb263525d3
Rafael Pinheiro Compto <50380578+rcompto@users.noreply.github.com>
2023-04-09 20:50:01 -0400

    Add files via upload

---
+++ client/install-blockchain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install git
+apt-get install curl
+apt-get install docker-compose
+systemctl enable docker
+usermod -a -G docker ubuntu
+wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzvf go1.20.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+cd /opt/
+git clone https://github.com/rcompto/blockchain.git
