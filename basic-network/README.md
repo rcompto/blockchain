002cba2eb4d9a5c4c5707eb429e962e6b60dea2d
Rafael Pinheiro Compto <50380578+rcompto@users.noreply.github.com>
2023-04-09 20:38:36 -0400

    Add files via upload

---
+++ basic-network/README.md
@@ -0,0 +1,13 @@
+## Basic Network Config
+
+Note as configuracoes usuadas para os certificados e chaves e
+algumas transacoes estao utilizando o channel "foxconn".
+
+Para gerar as chaves e arquivos de criptografia, ``generate.sh``.
+
+Para iniciar a blockchaim, ``start.sh``.
+Para parar ela, ``stop.sh``
+Para remover completamente todos os dados da rede criado no sistema,
+``teardown.sh``.
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>
---
+++ basic-network/start_peer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+sudo docker-compose -f docker-compose.yml down
+
+sudo docker-compose -f docker-compose.yml up -d peer1.org1.foxconn.com.br
+sudo docker ps -a
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=10
+#echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+# Create the channel
+sudo docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.foxconn.com.br/msp" peer1.org1.foxconn.com.br peer channel create -o orderer.foxconn.com.br:7050 -c foxconn -f /etc/hyperledger/configtx/channel.tx
+# Join peer1.org1.foxconn.com.br to the channel.
+sudo docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.foxconn.com.br/msp" peer1.org1.foxconn.com.br peer channel join -b foxconn.block
+
+#Creating CLI Container
+sudo docker-compose -f ./docker-compose.yml up -d cli2
